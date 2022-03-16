ea33b01a29d97b53c0b95dd3c1e188ca34ee3924
Talita Dovigo <88914279+TalitaGD@users.noreply.github.com>
2022-03-16 14:10:44 -0300

    Add files via upload

---
+++ trabalhandoComVariaveis.js
@@ -0,0 +1,8 @@
+//Console.log("Trabalhando com variaiveis"); = FORMA ERRADA!
+console.log("Trabalhando com variaiveis"); // FORMA CORRETA!
+// O JS é case sensitive!
+
+const idade = 20;
+const nome = "Talita";
+const ano = 2022;
+
